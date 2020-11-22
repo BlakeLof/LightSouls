ff68ba582f645f2cef43a018a026c3789f52e211
Aaron Harberson <aharberson1998@gmail.com>
2020-11-22 13:31:56 -0700

    Health System Update
    Re- Updated the Health System to fit more in line with our desires

---
+++ LightSouls/Assets/PlayerStats.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int healthLevel = 10;
+    public int maxHealth;
+    public int currentHealth;
+
+    public HealthBar healthbar;
+
+    void Start()
+    {
+        maxHealth = SetMaxHealthFromHealthLevel();
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+    private int SetMaxHealthFromHealthLevel()
+    {
+        maxHealth = healthLevel * 10;
+        return maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth = currentHealth - damage;
+
+        healthbar.SetCurrentHealth(currentHealth);
+    }
+}
